4e4179290bc4d419dceebf18a6c777ee028c84fb
kbh <kbh@DESKTOP-4FHEAFH>
2020-05-21 16:03:42 +0900

    jhj test!!

--- scrobot/src/main/java/scrobot/cmm/controller/cmm010101Controller.java
+++ scrobot/src/main/java/scrobot/cmm/controller/cmm010101Controller.java
@@ -31,8 +31,8 @@ import scrobot.cmm.service.cmm010101Service;
  * @see
  *
  *  Copyright (C) by MOPAS All right reserved.
- *  test
- *  test2 jhj
+ *  kbh test!!!
+ *  jhj test!!!
  */
 
 @Controller
